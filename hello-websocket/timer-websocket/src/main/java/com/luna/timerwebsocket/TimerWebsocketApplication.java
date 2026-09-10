cbc7eff22d6b93b35ed2ab86ced848e997412ca1
Hoseop.kim <kimho314@gmail.com>
2026-09-10 15:34:03 +0200

    [feat]: add hello websocket project

---
+++ hello-websocket/timer-websocket/src/main/java/com/luna/timerwebsocket/TimerWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.luna.timerwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimerWebsocketApplication.class, args);
+    }
+
+}
---
+++ hello-websocket/timer-websocket/src/test/java/com/luna/timerwebsocket/TimerWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luna.timerwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
