e5156b2885d59950c59dc2a0364152915b54ca8f
Hoseop.kim <kimho314@gmail.com>
2026-09-12 13:11:04 +0200

    [feat]: add direct chat project

---
+++ hello-websocket/direct-chat/src/main/java/com/luna/directchat/DirectChatApplication.java
@@ -0,0 +1,13 @@
+package com.luna.directchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectChatApplication.class, args);
+    }
+
+}
---
+++ hello-websocket/direct-chat/src/test/java/com/luna/directchat/DirectChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luna.directchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
